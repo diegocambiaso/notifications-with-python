a93ef3eb837cf98ef96878cf03bf17a1f8621378
Diego <diegocambiaso@gmail.com>
2023-03-03 16:42:44 -0500

    notificacion example in Py

---
+++ notifications.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+from plyer import bluetooth
+
+if __name__=="__main__":
+    while True:
+        notification.notify(
+            title="Pixelco!!!",
+            message="There are new news in technology ",
+            timeout=10
+        )
+        time.sleep(3600)
